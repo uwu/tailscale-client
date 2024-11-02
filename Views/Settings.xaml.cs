153ce87884e61371ee5f3124861759b1fa3aa1d4
xirreal <me@xirreal.dev>
2024-11-02 13:05:49 +0100

    Add version display in settings

--- Views/Settings.xaml.cs
+++ Views/Settings.xaml.cs
@@ -1,29 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.ApplicationModel;
 
 namespace TailscaleClient.Views;
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
+
 public sealed partial class Settings : Page
 {
+    private readonly string Version;
+
+    public static string GetAppVersion()
+    {
+        try
+        {
+            var version = Package.Current.Id.Version;
+            return string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
+        }
+        catch
+        {
+            return "Unpackaged build";
+        }
+    }
+
     public Settings()
     {
         this.InitializeComponent();
+        Version = GetAppVersion();
     }
 }
