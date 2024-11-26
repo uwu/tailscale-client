e42892a49938606c39261e5ac1428338ee77b906
redstonekasi <kasi@hanneskann.de>
2024-11-26 18:07:26 +0100

    WIP settings page

--- Views/Settings.xaml.cs
+++ Views/Settings.xaml.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using TailscaleClient.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,8 +13,44 @@ namespace TailscaleClient.Views;
 /// </summary>
 public sealed partial class Settings : Page
 {
+    private Core.Types.Prefs _prefs = null;
+
+    public bool IncomingTrafficEnabled = false;
+    public bool TailscaleDnsEnabled = false;
+    public bool SubnetRoutesEnabled = false;
+
+    public void UpdateUI()
+    {
+        _prefs = API.GetPrefs();
+        IncomingTrafficEnabled = !_prefs.ShieldsUp;
+        TailscaleDnsEnabled = _prefs.CorpDNS;
+        SubnetRoutesEnabled = _prefs.RouteAll;
+    }
+
     public Settings()
     {
-        this.InitializeComponent();
+        InitializeComponent();
+
+        Messaging.Instance.MessageReceived += (sender, e) =>
+        {
+            if (e.Kind == Messaging.MessageKind.IPNBusUpdate && e.Key == "Prefs")
+            {
+                DispatcherQueue.TryEnqueue(() => { UpdateUI(); });
+            }
+            else if (e.Kind == Messaging.MessageKind.ProfileSwitch)
+            {
+                DispatcherQueue.TryEnqueue(() => { UpdateUI(); });
+            }
+        };
+
+        UpdateUI();
+    }
+
+    private void UpdatePrefs(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        _prefs.ShieldsUp = !IncomingTrafficEnabled;
+        _prefs.CorpDNS = TailscaleDnsEnabled;
+        _prefs.RouteAll = SubnetRoutesEnabled;
+        API.EditPrefs(_prefs);
     }
 }
